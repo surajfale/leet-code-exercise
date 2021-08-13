ed8da27715184fe8d7d26500281891e563787c64
Suraj Fale <surajfale1@gmail.com>
2021-08-13 12:24:48 -0400

    Guess The Word Commit (#1)
    * Problem: GuessTheWord - https://leetcode.com/problems/guess-the-word/

--- src/test/scala/GuessTheWordTest.scala
+++ src/test/scala/GuessTheWordTest.scala
@@ -28,11 +28,26 @@ class GuessTheWordTest extends AnyFunSuite {
         assert(execute(inPutMap))
     }
 
+    test("Test Case 3") {
+        val inPutMap = mutable.HashMap("cymplm" -> Array("eykdft", "gjeixr", "eksbjm", "mxqhpk", "tjplhf", "ejgdra",
+            "npkysm", "jsrsid", "cymplm", "vegdgt", "jnhdvb", "jdhlzb", "sgrghh", "jvydne", "laxvnm", "xbcliw",
+            "emnfcw", "pyzdnq", "vzqbuk", "gznrnn", "robxqx", "oadnrt", "kzwyuf", "ahlfab", "zawvdf", "edhumz",
+            "gkgiml", "wqqtla", "csamxn", "bisxbn", "zwxbql", "euzpol", "mckltw", "bbnpsg", "ynqeqw", "uwvqcg",
+            "hegrnc", "rrqhbp", "tpfmlh", "wfgfbe", "tpvftd", "phspjr", "apbhwb", "yjihwh", "zgspss", "pesnwj",
+            "dchpxq", "axduwd", "ropxqf", "gahkbq", "yxudiu", "dsvwry", "ecfkxn", "hmgflc", "fdaowp", "hrixpl",
+            "czkgyp", "mmqfao", "qkkqnz", "lkzaxu", "cngmyn", "nmckcy", "alpcyy", "plcmts", "proitu", "tpzbok",
+            "vixjqn", "suwhab", "dqqkxg", "ynatlx", "wmbjxe", "hynjdf", "xtcavp", "avjjjj", "fmclkd", "ngxcal",
+            "neyvpq", "cwcdhi", "cfanhh", "ruvdsa", "pvzfyx", "hmdmtx", "pepbsy", "tgpnql", "zhuqlj", "tdrsfx",
+            "xxxyle", "zqwazc", "hsukcb", "aqtdvn", "zxbxps", "wziidg", "tsuxvr", "florrj", "rpuorf", "jzckev",
+            "qecnsc", "rrjdyh", "zjtdaw", "dknezk"))
+        assert(execute(inPutMap))
+    }
+
     def execute(inPutMap: mutable.HashMap[String, Array[String]]): Boolean = {
         var flag = false
         inPutMap.foreach(item => {
             val master = new Master(item._1, 10)
-            new GuessTheWord().findSecretWord(item._2, master)
+            GuessTheWord.findSecretWord(item._2, master)
             flag = master.foundWord
         })
         flag
